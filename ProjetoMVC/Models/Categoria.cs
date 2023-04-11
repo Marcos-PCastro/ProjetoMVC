aab09f99a511450374ccc97822e01f4bb09158a2
Marcos-PCastro <courrielprive@yahoo.com.br>
2023-04-11 11:52:02 -0300

    domain template creation

---
+++ ProjetoMVC/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjetoMVC.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
+
+
+}
