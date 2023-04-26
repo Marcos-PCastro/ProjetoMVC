f21df4a830560694e616c460a530456c4f74ace7
Marcos-PCastro <courrielprive@yahoo.com.br>
2023-04-26 12:34:15 -0300

    applying the Data Annotations attributes

--- ProjetoMVC/Models/Categoria.cs
+++ ProjetoMVC/Models/Categoria.cs
@@ -1,9 +1,20 @@
-﻿namespace ProjetoMVC.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ProjetoMVC.Models;
+
+[Table("Categorias")]
 public class Categoria
 {
+    [Key]
     public int CategoriaId { get; set; }
+    [StringLength(100, ErrorMessage = "O Tamanho máximo são 100 caracteres")]
+    [Required(ErrorMessage ="Informe o nome da categoria")]
+    [Display(Name ="Nome")]
     public string CategoriaNome { get; set; }
+    [StringLength(200, ErrorMessage = "O Tamanho máximo são 100 caracteres")]
+    [Required(ErrorMessage = "Informe a descrição da Categoria")]
+    [Display(Name = "Nome")]
     public string Descricao { get; set; }
 
 
