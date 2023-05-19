bc0fc2b8d165c7c99f0c78b7c619b22b62332c9b
Marcos-PCastro <courrielprive@yahoo.com.br>
2023-05-19 11:39:43 -0300

    populating the Categories and Snacks tables

---
+++ ProjetoMVC/Migrations/20230519140243_PopularCategorias.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjetoMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularCategorias : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) "
+                +  "VALUES('Normal','Lanche feito com ingredientes industrializados')");
+
+            migrationBuilder.Sql("INSERT INTO Categorias(CategoriaNome, Descricao) "
+                + "VALUES('Natural','Lanche feito com ingredientes naturais, ingtegrais e frescos')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Categorias");
+        }
+    }
+}
